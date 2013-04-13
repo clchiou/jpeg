90ed2229be82e2ee3521fd28534e7becde76cbcd
Che-Liang Chiou <clchiou@gmail.com>
2013-04-12 20:30:46 -0700

    Add copyright statement

--- nacl-jpeg.sh
+++ nacl-jpeg.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#
+# Copyright (c) 2013 Che-Liang Chiou. All rights reserved.
+# Use of this source code is governed by the GNU General Public License
+# as published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
 
 source pkg_info
 source ../common.sh
